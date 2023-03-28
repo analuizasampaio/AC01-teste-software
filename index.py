d87a3083a1643e6ceec74227bc3ff36a245a5806
Ana Luiza Sampaio <sampaioaanaluiza@gmail.com>
2023-03-28 20:54:27 -0300

    nomes

--- index.py
+++ index.py
@@ -1,3 +1,10 @@
+# Nome: Ana Luiza Sampaio RA: 2101183
+# Nome: Guilherme Portes Bebiano  RA: 2101494
+# Nome: Ingrid Priscila Alves de Sousa RA: 2101204
+# Nome: Charles Eduardo Felipe de Souza RA:  2100038
+# Nome: Kauê Ribeiro Varjão RA: 1904284
+# Nome: Sandra Silmara Santos RA:1904653
+
 dicionario = {'I': 1, "V": 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
 
 
